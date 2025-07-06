df45d417d152ddcc94c4cd21ebec9d55d27ff908
aditiaNovember01 <adtianovirman01@gmail.com>
2025-07-06 23:51:20 +0700

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <head>
         <meta property="og:title" content="Undangan Pernikahan Geri & Sisi" />
         <meta property="og:description" content="Yth. Bapak/Ibu/Saudara/i, Anda diundang ke pernikahan Geri & Sisi. Klik untuk detail & RSVP." />
+        <meta property="og:image" content="/images/bg2.jpeg" />
         <meta property="og:url" content="https://wedding-repository.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" type="image/png" href="/favicon.png" />
