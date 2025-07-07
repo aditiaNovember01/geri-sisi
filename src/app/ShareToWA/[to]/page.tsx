208f1f9cf322229a459828167c2cc74252adca50
aditiaNovember01 <adtianovirman01@gmail.com>
2025-07-07 14:53:23 +0700

    perbaikan

--- src/app/ShareToWA/[to]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import ShareToWA from "../../ShareToWA";
-
-export default function Page({ params }: { params: { to: string } }) {
-  const nama = decodeURIComponent(params.to || "");
-  return <ShareToWA initialNama={nama} />;
-} 
